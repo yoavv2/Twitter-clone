43ddc317866670ebc15542a7ddbdeebeae7d978b
yoav <23hutc8@gmail.com>
2022-02-15 20:57:53 +0200

    eslint

--- components/Modal.js
+++ components/Modal.js
@@ -1,28 +1,28 @@
-import { useRecoilState } from "recoil";
-import { modalState, postIdState } from "../atoms/modalAtom";
-import { Dialog, Transition } from "@headlessui/react";
-import { Fragment, useEffect, useState } from "react";
-import { useRouter } from "next/router";
-import Moment from "react-moment";
-import "emoji-mart/css/emoji-mart.css";
-import { Picker } from "emoji-mart";
-import { db } from "../firebase";
-import { useSession } from "next-auth/react";
+import { useRecoilState } from 'recoil';
+import { modalState, postIdState } from '../atoms/modalAtom';
+import { Dialog, Transition } from '@headlessui/react';
+import { Fragment, useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import Moment from 'react-moment';
+import 'emoji-mart/css/emoji-mart.css';
+import { Picker } from 'emoji-mart';
+import { db } from '../firebase';
+import { useSession } from 'next-auth/react';
 import {
   CalendarIcon,
   ChartBarIcon,
   EmojiHappyIcon,
   PhotographIcon,
   XIcon,
-} from "@heroicons/react/outline";
+} from '@heroicons/react/outline';
 import {
   addDoc,
   collection,
   doc,
   onSnapshot,
   serverTimestamp,
-} from "firebase/firestore";
-import Link from "next/link";
+} from 'firebase/firestore';
+import Link from 'next/link';
 
 function Modal() {
   const { data: session } = useSession();
@@ -30,12 +30,12 @@ function Modal() {
   const [postId, setPostId] = useRecoilState(postIdState);
 
   const [post, setPost] = useState({});
-  const [comment, setComment] = useState("");
+  const [comment, setComment] = useState('');
   const router = useRouter();
 
   useEffect(
     () =>
-      onSnapshot(doc(db, "posts", postId), (snapshot) => {
+      onSnapshot(doc(db, 'posts', postId), (snapshot) => {
         setPost(snapshot.data());
       }),
     [db]
@@ -44,7 +44,7 @@ function Modal() {
   const sendComment = async (e) => {
     e.preventDefault();
 
-    await addDoc(collection(db, "posts", postId, "comments"), {
+    await addDoc(collection(db, 'posts', postId, 'comments'), {
       comment: comment,
       username: session.user.name,
       tag: session.user.tag,
@@ -53,7 +53,7 @@ function Modal() {
     });
 
     setIsOpen(false);
-    setComment("");
+    setComment('');
     // setShowEmojis(false);
     router.push(`/${postId}`);
   };
@@ -63,72 +63,74 @@ function Modal() {
       show={isOpen}
       // as={Fragment}
     >
-      <Dialog as="div" className="fixed z-50 inset-0 pt-8" onClose={setIsOpen}>
-        <div className="flex items-start justify-center min-h-[800px] sm:min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+      <Dialog as='div' className='fixed z-50 inset-0 pt-8' onClose={setIsOpen}>
+        <div className='flex items-start justify-center min-h-[800px] sm:min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0'>
           <Transition.Child
             // as={Fragment}
-            enter="ease-out duration-300"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leave="ease-in duration-200"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
+            enter='ease-out duration-300'
+            enterFrom='opacity-0'
+            enterTo='opacity-100'
+            leave='ease-in duration-200'
+            leaveFrom='opacity-100'
+            leaveTo='opacity-0'
           >
-            <Dialog.Overlay className="fixed inset-0 bg-[#5b7083] bg-opacity-40 transition-opacity" />
+            <Dialog.Overlay className='fixed inset-0 bg-[#5b7083] bg-opacity-40 transition-opacity' />
           </Transition.Child>
 
           <Transition.Child
             // as={Fragment}
-            enter="ease-out duration-300"
-            enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-            enterTo="opacity-100 translate-y-0 sm:scale-100"
-            leave="ease-in duration-200"
-            leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-            leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            enter='ease-out duration-300'
+            enterFrom='opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95'
+            enterTo='opacity-100 translate-y-0 sm:scale-100'
+            leave='ease-in duration-200'
+            leaveFrom='opacity-100 translate-y-0 sm:scale-100'
+            leaveTo='opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95'
           >
             {/* ... */}
 
             <div
-              className="inline-block align-bottom bg-black rounded-2xl 
+              className='inline-block align-bottom bg-black rounded-2xl 
             text-left overflow-hidden shadow-xl transform transition-all 
-            sm:my-8 sm:align-middle sm:max-w-xl sm:w-full"
+            sm:my-8 sm:align-middle sm:max-w-xl sm:w-full'
             >
               <div
-                className="flex items-center px-1.5 py-2 border-b
-               border-gray-700"
+                className='flex items-center px-1.5 py-2 border-b
+               border-gray-700'
                 onClick={() => setIsOpen(false)}
               >
-                <div className="hoverAnimation w-9 h-9 flex items-center justify-center xl:px-0">
-                  <XIcon className="h-[22px] text-white" />
+                <div className='hoverAnimation w-9 h-9 flex items-center justify-center xl:px-0'>
+                  <XIcon className='h-[22px] text-white' />
                 </div>
               </div>
 
               <div
-                className="flex px-4 pt-5 pb-2.5 
-                sm:px-6"
+                className='flex px-4 pt-5 pb-2.5 
+                sm:px-6'
               >
-                <div className="w-full">
-                  <div className="text-[#6e767d] flex gap-x-3 relative">
-                    <span className="w-0.5 h-full z-[-1] absolute left-5 top-11 bg-gray-600" />
+                <div className='w-full'>
+                  <div className='text-[#6e767d] flex gap-x-3 relative'>
+                    <span className='w-0.5 h-full z-[-1] absolute left-5 top-11 bg-gray-600' />
+
+                    {/* eslint-disable-next-line @next/next/no-img-element */}
                     <img
                       src={post?.userImg}
-                      alt=""
-                      className="h-11 w-11 rounded-full"
+                      alt=''
+                      className='h-11 w-11 rounded-full'
                     />
                     <div>
-                      <div className="inline-block group">
-                        <h4 className="font-bold text-[#d9d9d9] inline-block text-[15px] sm:text-base">
+                      <div className='inline-block group'>
+                        <h4 className='font-bold text-[#d9d9d9] inline-block text-[15px] sm:text-base'>
                           {post?.username}
                         </h4>
-                        <span className="ml-1.5 text-sm sm:text-[15px]">
-                          @{post?.tag}{" "}
+                        <span className='ml-1.5 text-sm sm:text-[15px]'>
+                          @{post?.tag}{' '}
                         </span>
-                      </div>{" "}
-                      ·{" "}
-                      <span className="hover:underline text-sm sm:text-[15px]">
+                      </div>{' '}
+                      ·{' '}
+                      <span className='hover:underline text-sm sm:text-[15px]'>
                         <Moment fromNow>{post?.timestamp?.toDate()}</Moment>
                       </span>
-                      <p className="text-[#d9d9d9] text-[15px] sm:text-base">
+                      <p className='text-[#d9d9d9] text-[15px] sm:text-base'>
                         {post?.text}
                       </p>
                       {/* <p className="ml-1.5 text-xs sm:text-[15px]">
@@ -142,46 +144,47 @@ function Modal() {
                       </p> */}
                     </div>
                   </div>
-                  <div className="mt-7 flex space-x-3 w-full">
+                  <div className='mt-7 flex space-x-3 w-full'>
+                    {/* eslint-disable-next-line @next/next/no-img-element */}
                     <img
                       src={session.user.image}
-                      alt=""
-                      className="h-11 w-11 rounded-full"
+                      alt=''
+                      className='h-11 w-11 rounded-full'
                     />
-                    <form className="flex-grow mt-2">
+                    <form className='flex-grow mt-2'>
                       <textarea
-                        resize="none"
+                        resize='none'
                         value={comment}
                         onChange={(e) => setComment(e.target.value)}
-                        placeholder="Tweet your reply"
-                        rows="10"
-                        className="bg-transparent outline-none text-[#d9d9d9] text-lg placeholder-gray-500 tracking-wide w-full min-h-[80px] resize-none"
+                        placeholder='Tweet your reply'
+                        rows='10'
+                        className='bg-transparent outline-none text-[#d9d9d9] text-lg placeholder-gray-500 tracking-wide w-full min-h-[80px] resize-none'
                       />
-                      <div className="flex items-center justify-between pt-2.5">
-                        <div className="flex items-center">
-                          <div className="icon">
-                            <PhotographIcon className="text-[#1d9bf0] h-[22px]" />
+                      <div className='flex items-center justify-between pt-2.5'>
+                        <div className='flex items-center'>
+                          <div className='icon'>
+                            <PhotographIcon className='text-[#1d9bf0] h-[22px]' />
                           </div>
-                          <div className="icon rotate-90">
-                            <ChartBarIcon className="text-[#1d9bf0] h-[22px]" />
+                          <div className='icon rotate-90'>
+                            <ChartBarIcon className='text-[#1d9bf0] h-[22px]' />
                           </div>
-                          <div className="icon">
-                            <EmojiHappyIcon className="text-[#1d9bf0] h-[22px]" />
+                          <div className='icon'>
+                            <EmojiHappyIcon className='text-[#1d9bf0] h-[22px]' />
                           </div>
 
-                          <div className="icon">
-                            <CalendarIcon className="text-[#1d9bf0] h-[22px] " />
+                          <div className='icon'>
+                            <CalendarIcon className='text-[#1d9bf0] h-[22px] ' />
                           </div>
                         </div>
 
                         <button
-                          type="submit"
+                          type='submit'
                           onClick={sendComment}
                           disabled={!comment.trim()}
-                          className="bg-[#1d9bf0] 
+                          className='bg-[#1d9bf0] 
                         text-white rounded-full px-4 py-1.5 font-bold
                          shadow-md hover:bg-[#1a8cd8] disabled:hover:bg-[#1d9bf0]
-                          disabled:opacity-50 disabled:cursor-default"
+                          disabled:opacity-50 disabled:cursor-default'
                         >
                           Reply
                         </button>
--- pages/[id].js
+++ pages/[id].js
@@ -4,21 +4,21 @@ import {
   onSnapshot,
   orderBy,
   query,
-} from "@firebase/firestore";
-import { getProviders, getSession, useSession } from "next-auth/react";
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
-import { useRecoilState } from "recoil";
-import { modalState } from "../atoms/modalAtom";
-import Modal from "../components/Modal";
-import Sidebar from "../components/Sidebar";
-
-import Post from "../components/Post";
-import { db } from "../firebase";
-
-import Head from "next/head";
-import { ArrowLeftIcon } from "@heroicons/react/outline";
-import Comment from "../components/Comment";
+} from '@firebase/firestore';
+import { getProviders, getSession, useSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import { useRecoilState } from 'recoil';
+import { modalState } from '../atoms/modalAtom';
+import Modal from '../components/Modal';
+import Sidebar from '../components/Sidebar';
+import Login from '../components/Login';
+import Post from '../components/Post';
+import { db } from '../firebase';
+import Widgets from '../components/Widgets';
+import Head from 'next/head';
+import { ArrowLeftIcon } from '@heroicons/react/outline';
+import Comment from '../components/Comment';
 
 const PostPage = ({ trendingResults, followResults, providers }) => {
   const { data: session } = useSession();
@@ -31,22 +31,22 @@ const PostPage = ({ trendingResults, followResults, providers }) => {
 
   useEffect(
     () =>
-      onSnapshot(doc(db, "posts", id), (snapshot) => {
+      onSnapshot(doc(db, 'posts', id), (snapshot) => {
         setPost(snapshot.data());
       }),
-    [db]
+    [id]
   );
 
   useEffect(
     () =>
       onSnapshot(
         query(
-          collection(db, "posts", id, "comments"),
-          orderBy("timestamp", "desc")
+          collection(db, 'posts', id, 'comments'),
+          orderBy('timestamp', 'desc')
         ),
         (snapshot) => setComments(snapshot.docs)
       ),
-    [db, id]
+    [id]
   );
 
   if (!session) return <Login providers={providers} />;
@@ -55,25 +55,27 @@ const PostPage = ({ trendingResults, followResults, providers }) => {
     <div>
       <Head>
         <title>
-          {post?.username} on Twitter: "{post?.text}"
+          ` ${post?.username} on Twitter: "${post?.text}"`
         </title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel='icon' href='/favicon.ico' />
       </Head>
-      <main className="bg-black min-h-screen flex max-w-[1500px] mx-auto">
+      <main className='bg-black min-h-screen flex max-w-[1500px] mx-auto'>
         <Sidebar />
-        <div className="flex-grow border-l border-r border-gray-700 max-w-2xl sm:ml-[73px] xl:ml-[370px]">
-          <div className="flex items-center px-1.5 py-2 border-b border-gray-700 text-[#d9d9d9] font-semibold text-xl gap-x-4 sticky top-0 z-50 bg-black">
+        <div className='flex-grow border-l border-r border-gray-700 max-w-2xl sm:ml-[73px] xl:ml-[370px]'>
+          <div className='flex items-center px-1.5 py-2 border-b border-gray-700 text-[#d9d9d9]
+           font-semibold text-xl gap-x-4 sticky top-0 z-50 bg-black'>
             <div
-              className="hoverAnimation w-9 h-9 flex items-center justify-center xl:px-0"
-              onClick={() => router.push("/")}
+              className='hoverAnimation w-9 h-9 flex items-center justify-center xl:px-0'
+              onClick={() => router.push('/')}
             >
-              <ArrowLeftIcon className="h-5 text-white" />
+              <ArrowLeftIcon className='h-5 text-white' />
             </div>
             Tweet
           </div>
+
           <Post id={id} post={post} postPage />
           {comments.length > 0 && (
-            <div className="pb-72">
+            <div className='pb-72'>
               {comments.map((comment) => (
                 <Comment
                   key={comment.id}
@@ -84,6 +86,10 @@ const PostPage = ({ trendingResults, followResults, providers }) => {
             </div>
           )}
         </div>
+        <Widgets
+          trendingResults={trendingResults}
+          followResults={followResults}
+        />
 
         {isOpen && <Modal />}
       </main>
@@ -94,10 +100,10 @@ const PostPage = ({ trendingResults, followResults, providers }) => {
 export default PostPage;
 
 export async function getServerSideProps(context) {
-  const trendingResults = await fetch("https://jsonkeeper.com/b/NKEV").then(
+  const trendingResults = await fetch('https://jsonkeeper.com/b/NKEV').then(
     (res) => res.json()
   );
-  const followResults = await fetch("https://jsonkeeper.com/b/WWMJ").then(
+  const followResults = await fetch('https://jsonkeeper.com/b/WWMJ').then(
     (res) => res.json()
   );
   const providers = await getProviders();
