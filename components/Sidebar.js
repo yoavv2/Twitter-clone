66268fdbe8db8dc49a2e0d4453941a751751e2b7
yoavv2 <23hutc8@gmail.com>
2023-02-01 17:53:06 +0200

    .

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,4 +1,3 @@
-/* eslint-disable jsx-a11y/alt-text */
 import SidebarLink from './SidebarLink';
 import { HomeIcon } from '@heroicons/react/solid';
 import { useSession, signOut } from 'next-auth/react';
@@ -17,8 +16,10 @@ import { useRouter } from 'next/router';
 function Sidebar() {
   const { data: session } = useSession();
   const router = useRouter();
+
+  const pathname = router.pathname;
   return (
-    <section className='hidden sm:flex flex-col items-center xl:items-start xl:w-[340px] p-2 fixed h-full'>
+    <nav className='hidden sm:flex flex-col items-center xl:items-start xl:w-[340px] p-2 fixed h-full'>
       <div
         className='flex items-center justify-center w-14 h-14 hoverAnimation p-0 xl:ml-24'
         onClick={() => router.push('/')}
@@ -30,23 +31,42 @@ function Sidebar() {
           className='h-10 w-10'
         />
       </div>
-      <div className='space-y-2.5 mt-4 mb-2.5 xl:ml-24'>
-        <div onClick={() => router.push('/')}>
-          <SidebarLink text='Home' Icon={HomeIcon} active />
-        </div>
-        <SidebarLink text='Explore' Icon={HashtagIcon} />
-        <SidebarLink text='Notifications' Icon={BellIcon} />
-        <SidebarLink text='Messages' Icon={InboxIcon} />
-        <SidebarLink text='Bookmarks' Icon={BookmarkIcon} />
-        <SidebarLink text='Lists' Icon={ClipboardListIcon} />
-        <SidebarLink text='Profile' Icon={UserIcon} />
-        <SidebarLink text='More' Icon={DotsCircleHorizontalIcon} />
-      </div>
+      <ul className='space-y-2.5 mt-4 mb-2.5 xl:ml-24'>
+        <li>
+          <SidebarLink
+            text='Home'
+            Icon={HomeIcon}
+            active={pathname === '/'}
+            linkTo={'/'}
+          />
+        </li>
+        <li>
+          <SidebarLink text='Explore' Icon={HashtagIcon} />
+        </li>
+        <li>
+          <SidebarLink text='Notifications' Icon={BellIcon} />
+        </li>
+        <li>
+          <SidebarLink text='Messages' Icon={InboxIcon} />
+        </li>
+        <li>
+          <SidebarLink text='Bookmarks' Icon={BookmarkIcon} />
+        </li>
+        <li>
+          <SidebarLink text='Lists' Icon={ClipboardListIcon} />
+        </li>
+        <li>
+          <SidebarLink text='Profile' Icon={UserIcon} />
+        </li>
+        <li>
+          <SidebarLink text='More' Icon={DotsCircleHorizontalIcon} />
+        </li>
+      </ul>
       <button className='hidden xl:inline ml-auto bg-[#1d9bf0] text-white rounded-full w-56 h-[52px] text-lg font-bold shadow-md hover:bg-[#1a8cd8]'>
         Tweet
       </button>
-      <div
-        className='text-[#d9d9d9] flex items-center justify-center mt-auto hoverAnimation xl:ml-auto xl:-mr-5'
+      <button
+        className='w-56 h-[52px] text-[#d9d9d9] flex items-center justify-between mt-auto hoverAnimation xl:ml-auto xl:-mr-5'
         onClick={signOut}
       >
         {/* eslint-disable-next-line @next/next/no-img-element  */}
@@ -60,8 +80,8 @@ function Sidebar() {
           <p className='text-[#6e767d]'>@{session?.user.tag}</p>
         </div>
         <DotsHorizontalIcon className='h-5 hidden xl:inline ml-10' />
-      </div>
-    </section>
+      </button>
+    </nav>
   );
 }
 
--- components/SidebarLink.js
+++ components/SidebarLink.js
@@ -1,16 +1,19 @@
-function SidebarLink({ text, Icon, active }) {
+import Link from 'next/link';
+
+function SidebarLink({ text, Icon, active, linkTo = '/' }) {
   return (
-    <div
-      className={`text-[#d9d9d9] flex items-center justify-center
-    xl:justify-start text-xl space-x-3 hoverAnimation ${
-      active ? 'font-bold' : ''
-    }`}
-    >
-      <Icon className='h-7' />
-      <span className={`hidden xl:inline ${active ? 'text-bold' : ''}`}>
-        {text}
-      </span>
-    </div>
+    <Link href={linkTo}>
+      <a
+        className={`text-[#d9d9d9] flex items-center justify-center
+           xl:justify-start text-xl space-x-3 hoverAnimation 
+           ${active ? 'font-bold' : ''}`}
+      >
+        <Icon className='h-7' />
+        <span className={`hidden xl:inline ${active ? 'text-bold' : ''}`}>
+          {text}
+        </span>
+      </a>
+    </Link>
   );
 }
 
